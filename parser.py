2d7106875ae328859d8396e27313ddf853f4d2ce
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2017-04-13 11:35:05 +0200

    Improve unknown cycles scores detection

--- parser.py
+++ parser.py
@@ -221,7 +221,7 @@ def parse_wiki():
     
     table_reg = re.compile(r'^\|{0}\|{0}\|{0}\|{0}\|{0}\|?{0}?\|?{0}?$'.format(r'([^|]+)'))
     level_reg = re.compile(r'^(?P<level>.+?)(?: - (?P<OS>Windows|Linux|Unknown OS))?(?: - (?P<reactors>\d) Reactors?)?\s*$')
-    score_reg = re.compile(r'^\s*\[?\(\**(?P<cycles>\?|\d+)\**(?P<OSmark>\\\*)?/\**(?P<reactors>\d+)\**/\**(?P<symbols>\d+)\**\)'
+    score_reg = re.compile(r'^\s*\[?\(\**(?P<cycles>[?\d]+)\**(?P<OSmark>\\\*)?/\**(?P<reactors>\d+)\**/\**(?P<symbols>\d+)\**\)'
                            r'\s+(?P<user>[^\]]+?)(?:\]\((?P<link>[^\)]+)\).*?)?\s*$')
     single_score_reg = re.compile(r'^\s*\**(?P<score>\d+)\**\s*$')
     
@@ -245,7 +245,7 @@ def parse_wiki():
                             username = score_match.group('OSmark') + score_match.group('user')
                         else:
                             username = score_match.group('user')
-                        if score_match.group('cycles') == '?':
+                        if '?' in score_match.group('cycles'):
                             cycles = 99999999
                         else:
                             cycles = int(score_match.group('cycles'))
