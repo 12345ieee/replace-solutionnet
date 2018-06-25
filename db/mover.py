79f1ef0746399589e19db3c2e6b0bea8bd58baab
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2018-06-25 23:42:26 +0200

    Recover piping

--- db/mover.py
+++ db/mover.py
@@ -1,22 +1,56 @@
 #!/usr/bin/env python3
 
-import collections
 import argparse
+import collections
+import csv
 import sqlite3
 
 import psycopg2
 import psycopg2.extras
 
-replace_old_score = True
-
-Pipe = collections.namedtuple('Pipe', ['output', 'x', 'y'])
+Point = collections.namedtuple('Point', ['x', 'y'])
 
 sn_cur = None
 sv_cur = None
 seq_comp = None
-seq_memb = None 
+seq_memb = None
+seeds = {}
 
-def load_solution(sol_id):
+def reorder_pipe(pipe, seed):
+    """Bounds: -24;30;-18;21"""
+    x_size = 56
+    y_size = 41
+    field = [[0]*y_size for _ in range(x_size)]
+    for pt in pipe:
+        field[pt.x][pt.y] = 1
+    
+    output = [seed]
+    if field[seed.x][seed.y] == 0:
+        # bad stuff happened
+        print(f'Seed {seed} not found')
+        return output
+    
+    prev = None
+    curr = seed
+    while True:
+        adiacence = [(curr.x+dx, curr.y+dy, field[curr.x+dx][curr.y+dy]) for dx,dy in [(1,0), (-1,0), (0,1), (0,-1)]]
+        s = sum(val[2] for val in adiacence)
+        if prev is None and s == 1:
+            # starting, good case
+            prev,curr = curr,Point(*([(x,y) for x,y,o in adiacence if o == 1][0]))
+            output.append(curr)
+        elif prev is not None and s == 2:
+            # continuing, good case
+            prev,curr = curr,Point(*([(x,y) for x,y,o in adiacence if o == 1 and not (x == prev.x and y == prev.y)][0]))
+            output.append(curr)
+        else:
+            # see if we've finished all the pipes
+            if len(output) != len(pipe):
+                print('Incomplete piping')
+            return output
+    
+
+def load_solution(sol_id, replace_old_score):
     global seq_comp
     global seq_memb
     # get the level and the score
@@ -25,6 +59,9 @@ def load_solution(sol_id):
                         where s.level_id = l.level_id
                           and s.solution_id = %s;''', (sol_id,))
     solution = sn_cur.fetchone()
+    if solution is None:
+        print(f'ERROR: Invalid solution id: {sol_id}')
+        exit()
     int_level_name = solution['internal_name']
     triplet = solution[1:]
     
@@ -34,7 +71,8 @@ def load_solution(sol_id):
     if count == 0:
         # need to insert the level
         print(f'Adding new solution to {int_level_name}')
-        sv_cur.execute(r"""INSERT INTO Level VALUES (?, 1, 0, ?, ?, ?, ?, ?, ?)""", [int_level_name, *triplet, *triplet])
+        sv_cur.execute(r"""INSERT INTO Level
+                           VALUES (?, 1, 0, ?, ?, ?, ?, ?, ?)""", [int_level_name, *triplet, *triplet])
     else:
         # there's already a solution (possibly empty) in the file
         if not replace_old_score:
@@ -69,10 +107,16 @@ def load_solution(sol_id):
                                   [seq_comp, int_level_name, reactor['type'], reactor['x'], reactor['y']])
         
         # get all its pipes
-        #~ sn_cur.execute(r"select * from pipes where component_id = %s;", (comp_id,))
-        #~ pipes = []
-        #~ for pipe in sn_cur:
-            #~ sv_cur.execute(r"INSERT INTO Pipe VALUES (?, ?, ?, ?)", [seq_comp, pipe['output'], pipe['x'], pipe['y']])
+        sn_cur.execute(r"select * from pipes where component_id = %s;", (comp_id,))
+        pipes = ([], [])
+        for pipe_point in sn_cur:
+            pipes[pipe_point['output_id']].append(Point(pipe_point['x'], pipe_point['y']))
+        for out_id, pipe in enumerate(pipes):
+            if not pipe:
+                continue
+            reordered_pipe = reorder_pipe(pipe, seeds[(reactor['type'], out_id)])
+            for pipe_point in reordered_pipe:
+                sv_cur.execute(r"INSERT INTO Pipe VALUES (?, ?, ?, ?)", (seq_comp, out_id, pipe_point.x, pipe_point.y))
         
         # get all its symbols
         sn_cur.execute(r"select * from members where component_id = %s;", (comp_id,))
@@ -82,25 +126,33 @@ def load_solution(sol_id):
                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
                                [seq_memb, seq_comp] + symbol[2:])
 
-def main(save_path, sol_ids):
+def main(args):
     global sn_cur
     global sv_cur
     global seq_comp
     global seq_memb
+    global seeds
     
+    # connections
     sn_conn = psycopg2.connect(dbname='solutionnet')
     sn_cur = sn_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     
-    sv_conn = sqlite3.connect(save_path)
+    sv_conn = sqlite3.connect(args.savefile)
     sv_cur = sv_conn.cursor()
     
     # get the sequences
     seqs = sv_cur.execute('select seq from sqlite_sequence order by 1')
     seq_comp, seq_memb = (int(seq[0]) for seq in seqs)
     
-    for sol_id in sol_ids:
+    # populate seeds map
+    with open('seeds.csv') as levelscsv:
+        reader = csv.DictReader(levelscsv, skipinitialspace=True)
+        for row in reader:
+            seeds[(row['type'], int(row['output']))] = Point(int(row['x']), int(row['y']))
+    
+    for sol_id in args.sol_ids:
         print(f'Loading solution {sol_id}')
-        load_solution(sol_id)
+        load_solution(sol_id, args.replace_scores)
     
     # write sequences
     sv_cur.execute(fr"""UPDATE sqlite_sequence
@@ -116,7 +168,8 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--savefile", action="store", default=r'saves/111.user')
-    parser.add_argument("sol_ids", nargs='*', type=int, default=47424)
+    parser.add_argument("--no-replace-scores", action="store_false", dest='replace_scores')
+    parser.add_argument("sol_ids", nargs='*', type=int, default=[47424])
     args = parser.parse_args()
 
-    main(args.savefile, args.sol_ids)
+    main(args)
