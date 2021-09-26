9cf23640bfcea23e87c774f93546ea4145d49dc3
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2021-09-26 07:25:52 +0200

    Meet pipes in the middle, missing 165627 now

--- mover_solnet.py
+++ mover_solnet.py
@@ -15,41 +15,79 @@ Point = collections.namedtuple('Point', ['x', 'y'])
 
 seeds = {}
 
+def find_neighbours(curr, field):
+    return [Point(curr.x+dx, curr.y+dy) for dx,dy in [(1,0), (-1,0), (0,1), (0,-1)]
+                                        if field[curr.x+dx][curr.y+dy] == 'p']
+
 def reorder_pipe(pipe, seed):
-    """Bounds: -24;30;-18;21"""
+
+    def print_board(header):
+        print(header + '\n' + '\n'.join(l for l in (''.join(r[-18:] + r[:-18])
+                                                    for r in field[-24:] + field[:-24])
+                                          if l != '.' * 41))
+
+    # Bounds: -24;30;-18;21
     x_size = 56
     y_size = 41
     field = [['.']*y_size for _ in range(x_size)]
     for pt in pipe:
         field[pt.x][pt.y] = 'p'
 
-    def find_neighbours(curr):
-        return [Point(curr.x+dx, curr.y+dy) for dx,dy in [(1,0), (-1,0), (0,1), (0,-1)]
-                                            if field[curr.x+dx][curr.y+dy] == 'p']
+    # check if the pipe end is alone, so we can meet the pipe in the middle
+    other_seed = None
+    for pt in pipe:
+        if len(find_neighbours(pt, field)) == 1 and pt != seed:
+            other_seed = pt
+            field[other_seed.x][other_seed.y] = 'e'
+            break
 
-    def print_board(header, field):
-        print(header + '\n' + '\n'.join(l for l in (''.join(r[-18:] + r[:-18])
-                                                    for r in field[-24:] + field[:-24])
-                                          if l != '.' * 41))
-
-    curr = seed
-    output = [seed]
+    # mark the seed after we've found the other seed
     field[seed.x][seed.y] = 's'
+
+    if not other_seed:
+        # do it on one side, 2k tries
+        output = build_pipe(field, [seed], len(pipe), None, 2000)
+        if len(output) != len(pipe):
+            print_board(f'Incomplete piping ({len(output)}, {len(pipe) - len(output)})')
+        return output
+    else:
+        # try meeting in the middle, 5*2*200 tries
+        out_forward = [seed]
+        out_backward = [other_seed]
+        for i in range(6):
+            out_forward = build_pipe(field, out_forward, len(pipe) - len(out_backward),
+                                     out_backward[-1], 200, clean=True)
+            out_backward = build_pipe(field, out_backward, len(pipe) - len(out_forward),
+                                      out_forward[-1], 200, clean=i != 5)
+            if len(out_forward) + len(out_backward) == len(pipe):
+                return out_forward + out_backward[::-1]
+        print_board(f'Incomplete piping ({len(out_forward)}, {len(pipe) - len(out_forward)})')
+        return out_forward
+
+
+def build_pipe(field, starting_output, target_len, target_point=None, iterations=2000, clean=False) -> list:
+
+    def is_neighbour(pt1: Point, pt2: Point) -> bool:
+        return abs(pt1.x - pt2.x) <= 1 and abs(pt1.y - pt2.y) <= 1
+
+    curr = starting_output[-1]
+    output = starting_output[:]
     backtracking_stack = []
     backtracking_counter = 0
     while True:
-        neighbours = find_neighbours(curr)
+        neighbours = find_neighbours(curr, field)
         if len(neighbours) == 0:
             # see if we've finished all the pipes
-            if len(pipe) == len(output):
+            if len(output) == target_len and (not target_point or is_neighbour(output[-1], target_point)):
                 return output
 
             # know when to fold em
             backtracking_counter += 1
-            if backtracking_counter == 2000:
-                res = output[:-len(backtracking_stack)]
-                print_board(f'Incomplete piping ({len(res)}, {len(pipe) - len(res)})', field)
-                return res
+            if backtracking_counter == iterations:
+                if clean:
+                    for pt in output[-len(backtracking_stack):]: # clean up temps for next run
+                        field[pt.x][pt.y] = 'p'
+                return output[:-len(backtracking_stack)]
 
             # find divergence point
             while not neighbours:
